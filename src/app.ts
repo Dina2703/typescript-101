90cef0f9b4191fadd6ff66bbbbd04d4e84d0df7c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-11 22:05:12 -0400

    DOM & Type Casting

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+const anchor = document.querySelector("a")!;
+
+// if (anchor) {
+//   console.log(anchor.href);
+// }
+// or we can add '!' mark at the end of the querySelector
+console.log(anchor.href);
+//TYPE CASTING
+//when you grab tag itself, TS knows what what tag it is and all of the properties and methods are available for us. But when you grab class name better specify the tag name, by adding 'as', like '... as HTMLFormElement', in order to help TS recognize the tag and get access to all the props and method of it.
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
+
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
