cc0a1bec42578a8b398bbc5fbdce845500569f6a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-12 21:47:59 -0400

    Rendering an HTML template

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,15 @@
-// //Interfaces
-// interface IsPerson {
-//   name: string;
-//   age: number;
-//   speak(a: string): void;
-//   spend(a: number): number;
-// }
-
-// const me: IsPerson = {
-//   name: "sam",
-//   age: 30,
-//   speak(text: string): void {
-//     console.log(text);
-//   },
-//   spend(amount: number): number {
-//     console.log(`I spend ${amount}`);
-//     return amount;
-//   },
-// };
-
-// console.log(me);
-
 import { Payment } from "./classes/Payments.js";
 import { Invoice } from "./classes/Invoice.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-// const anchor = document.querySelector("a")!;
+import { ListTemplate } from "./classes/ListTemplate.js";
 
-//usage of a interface
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("Sami", "web work", 250);
-// docTwo = new Payment("Mario", "data-analitic", 300);
-
-// let docs: HasFormatter[] = [];
-
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// if (anchor) {
-//   console.log(anchor.href);
-// }
-// or we can add '!' mark at the end of the querySelector
-// console.log(anchor.href);
-//TYPE CASTING
-//when you grab tag itself, TS knows what what tag it is and all of the properties and methods are available for us. But when you grab class name better specify the tag name, by adding 'as', like '... as HTMLFormElement', in order to help TS recognize the tag and get access to all the props and method of it.
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-// console.log(form.children);
+
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
@@ -59,52 +21,10 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  //list tempalte instance
+  const ul = document.querySelector("ul")!;
+  const list = new ListTemplate(ul);
 
-  // console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  // console.log(doc);
+  list.render(doc, type.value, "end");
 });
-
-//inputs
-const type = document.querySelector("#type") as HTMLSelectElement;
-const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
-
-//3 modifiers:
-//PUBLIC which is default one - we can change and read the value outside of the class as well as inside of the class;
-// PRIVATE allows us to read and only change inside the class(inside a method).
-//READONLY allows us to read it both inside the calss and outside the class.
-
-//CLASSES
-//version #1
-// class Invoice {
-//   readonly client: string;
-//   public details: string;
-//   private amount: number;
-
-//   //initialize the values like 'c: string', then assign them to the props above, like 'this.client = c';
-//   constructor(c: string, d: string, a: number) {
-//     this.client = c;
-//     this.details = d;
-//     this.amount = a;
-//   }
-
-//   //define methods of the Invoice obj.
-//   format() {
-//     return `${this.client} owes $${this.amount} for ${this.details}`;
-//   }
-// }
-
-// const invOne = new Invoice("Bob", "work on the park", 200);
-// const invTwo = new Invoice("Sam", "work on the pool", 300);
-
-// console.log(invOne, invTwo);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// console.log(invoices);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.format());
-// });
